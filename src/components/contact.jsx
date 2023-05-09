7e3cc928d6c7a0338bbff9dd33cde6230062effc
Zarrar Palekar <zarrar.palekar@azularc.com>
2023-05-09 18:18:39 +0530

    recent changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -126,17 +126,17 @@ export const Contact = (props) => {
               <div className="social">
                 <ul>
                   <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
+                    <a href={props.data ? `//${props.data.facebook}` : "/"}>
                       <i className="fa fa-facebook"></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
+                    <a href={props.data ? `//${props.data.twitter}` : "/"}>
                       <i className="fa fa-twitter"></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
+                    <a href={props.data ? `//${props.data.youtube}` : "/"}>
                       <i className="fa fa-youtube"></i>
                     </a>
                   </li>
@@ -149,9 +149,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2023 SM Classes Developed by{" "}
+            <a href="https://www.zarrarpalekar.com" rel="nofollow">
+              Zarrar Palekar
             </a>
           </p>
         </div>
