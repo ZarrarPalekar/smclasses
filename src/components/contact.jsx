21ceb1fbca945a0f6630e55df8927f95425cf404
Zarrar Palekar <zarrar@live.in>
2025-06-08 08:32:19 +0530

    fix footer

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -156,7 +156,7 @@ export const Contact = (props) => {
       <footer className="footer-section">
         <div className="container text-center">
           <p>
-            &copy; 2023 SM Classes Developed by{" "}
+            &copy; 2025 SM Classes Developed by{" "}
             <a href="https://www.zarrarpalekar.com" rel="nofollow">
               Zarrar Palekar
             </a>
